bc36b245c22495cadaaffef6655fafe1c7d31e11
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-07-22 17:48:32 -0300

    Removendo arquivos desnecessários

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <p>Home</p>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from "vue";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default Vue.extend({
+  name: "Home"
+});
 </script>
+
+<style lang="scss" scoped>
+</style>
