80cbd0b96cfcafe14ba7ab11495c2c0003fae9d0
Felipe Rocha de Lucena - FLC <felipe.lucena@iteris.com.br>
2020-07-26 12:53:45 -0300

    Finalizando estilo do header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,22 +2,33 @@
   <header>
     <v-container class="d-flex justify-space-between py-4">
       <v-col class="d-flex align-center">
-        <img
-          :src="require('@/assets/projeto-faca-o-bem-64.png')"
-          alt="Logo Projeto Faça o Bem"
-          class="mr-2"
-        />
-        <h1>Projeto Faça o Bem</h1>
+        <router-link to="/">
+          <img
+            :src="require('@/assets/projeto-faca-o-bem-32.png')"
+            alt="Logo Projeto Faça o Bem"
+            class="mr-2"
+          />
+        </router-link>
+        <router-link to="/">
+          <h1>Projeto Faça o Bem</h1>
+        </router-link>
       </v-col>
-      <v-col class="d-flex justify-space-between">
-        <ul>
-          <li>
-            <router-link to="/">Sobre</router-link>
-            <router-link to="/">Como ajudar</router-link>
-            <router-link to="/">Fale Conosco</router-link>
-            <router-link to="/">Doe Agora</router-link>
-          </li>
-        </ul>
+      <v-col class="d-flex align-center">
+        <nav class="ml-auto nav-list d-flex align-center">
+          <router-link to="/">Sobre</router-link>
+          <router-link to="/">Como ajudar</router-link>
+          <router-link to="/">Fale Conosco</router-link>
+          <router-link to="/">
+            <v-btn
+              id="btn-doar"
+              color="#4db055"
+              height="38px"
+              link="true"
+              to="/"
+              >Doe Agora</v-btn
+            >
+          </router-link>
+        </nav>
       </v-col>
     </v-container>
   </header>
@@ -36,6 +47,20 @@ header {
   background-color: $cinza-claro-3;
 }
 h1 {
-  @include dosis($base-5, $s24);
+  @include dosis($base-5, $s18);
+}
+
+.nav-list .router-link-active {
+  @include dosis($cinza-escuro-2, $s18);
+  padding: 0 16px;
+}
+
+.nav-list .router-link-exact-active {
+  color: $base-5;
+}
+
+#btn-doar {
+  @include dosis($cinza-claro-1, $s14);
+  text-transform: none;
 }
 </style>
